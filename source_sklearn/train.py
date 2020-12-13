d6435fb2d2b6ffa7d6f0099871cec5236906427b
Deasy Indrawati <indrawatideasy@gmail.com>
2020-12-13 23:40:33 +0700

    Add files via upload

--- source_sklearn/train.py
+++ source_sklearn/train.py
@@ -4,34 +4,33 @@ import argparse
 import os
 import pandas as pd
 
-# sklearn.externals.joblib is deprecated in 0.21 and will be removed in 0.23. 
-# from sklearn.externals import joblib
+# sklearn.externals.joblib is deprecated in 0.21 and will be removed in 0.23.
+from sklearn.externals import joblib
 # Import joblib package directly
-import joblib
+# import joblib
 
-## TODO: Import any additional libraries you need to define a model
-
+# TODO: Import any additional libraries you need to define a model
+from sklearn import svm
 
 # Provided model load function
+
 def model_fn(model_dir):
     """Load model from the model_dir. This is the same model that is saved
     in the main if statement.
     """
     print("Loading model.")
-    
-    # load using joblib
+
+# load using joblib
     model = joblib.load(os.path.join(model_dir, "model.joblib"))
     print("Done loading model.")
-    
+
     return model
-
-
-## TODO: Complete the main code
+# TODO: Complete the main code
 if __name__ == '__main__':
-    
+
     # All of the model parameters and training parameters are sent as arguments
     # when this script is executed, during a training job
-    
+
     # Here we set up an argument parser to easily access the parameters
     parser = argparse.ArgumentParser()
 
@@ -40,9 +39,9 @@ if __name__ == '__main__':
     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
-    
+
     ## TODO: Add any additional arguments that you will need to pass into your model
-    
+
     # args holds all passed-in arguments
     args = parser.parse_args()
 
@@ -51,23 +50,19 @@ if __name__ == '__main__':
     train_data = pd.read_csv(os.path.join(training_dir, "train.csv"), header=None, names=None)
 
     # Labels are in the first column
-    train_y = train_data.iloc[:,0]
-    train_x = train_data.iloc[:,1:]
-    
-    
-    ## --- Your code here --- ##
-    
+    train_y = train_data.iloc[:, 0]
+    train_x = train_data.iloc[:, 1:]
 
-    ## TODO: Define a model 
-    model = None
-    
-    
+    ## --- Your code here --- ##
+
+## TODO: Define a model
+    model = svm.SVC(kernel='linear')
+
     ## TODO: Train the model
-    
-    
-    
-    ## --- End of your code  --- ##
-    
+    model.fit(train_x, train_y)
+
+## --- End of your code  --- ##
 
     # Save the trained model
     joblib.dump(model, os.path.join(args.model_dir, "model.joblib"))
+    
